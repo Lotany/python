9557bc93934acbb2310d89a001a9f8a2a2fb5eb2
Able <ablekilinda@gmail.com>
2022-11-30 03:12:46 +0300

    complete number guessing game

---
+++ number_guessing/number.py
@@ -0,0 +1,34 @@
+#random number
+import random
+
+top_of_range = input("type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range =int(top_of_range)
+    if top_of_range <=0:
+        print("Please type a number larger than zero!")
+        quit()
+else:
+    print("Please enter a number next time!")
+    quit()
+
+random_number =random.randint(0, top_of_range)
+
+guess =0 
+while True:
+    guess +=1
+    user_guess =input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time')
+        continue
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You are above the number: ")
+    else:
+        print("Youb are below the number: ")
+
+print(f"You got it in {guess} guesses")
