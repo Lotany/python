3489656074ac2233a567bf6807da42c2415edbd9
Able <ablekilinda@gmail.com>
2022-12-01 20:52:34 +0300

    complete password manager project

--- password_manager/password.py
+++ password_manager/password.py
@@ -3,18 +3,18 @@ from cryptography.fernet import Fernet
 
 def write_key():
     key = Fernet.generate_key()
-    with open("Password_manager/key.key","Wb") as key_file:
+    with open('password_manager/key.key','Wb') as key_file:
         key_file.write(key)
 
 
 def load_key():
-    file = open("password_manager/key.key","rb")
+    file = open('password_manager/key.key','rb')
     key = file.read()
     file.close()
     return key
 
 pwd = input ("What is the master password? ")
-key =load_key() + pwd.bytes
+key =load_key() + pwd.encode()
 fer =Fernet(key)
 
 def view():
@@ -22,13 +22,13 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("user: ", user, "| Password: ",  str(fer.encrypt(passw.encode())))
+            print("user: ", user, "| Password: ", fer.decrypt(passw.encode()).decode())
 
 def add():
     user = input("Ente name: ")
     passw = input("Password: ")
     with open('password_manager/password.txt', 'a') as f:
-        f.write(user+ " | "+ str(fer.encrypt(passw.encode())) + "\n")
+        f.write(user+ " | "+ fer.encrypt(passw.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to ad a new password or view existing ones(view/add/q) ").lower()
