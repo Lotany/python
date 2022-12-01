78cafbe5eae17904da3653928c8f5f4e76f2438b
Able <ablekilinda@gmail.com>
2022-12-01 03:59:10 +0300

    error no such directory

--- password_manager/password.py
+++ password_manager/password.py
@@ -1,19 +1,34 @@
-pwd = input ("What is the master password? ")
+from cryptography.fernet import Fernet
 
-#pip install cryptography
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("Password_manager/key.key","Wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    file = open("password_manager/key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+pwd = input ("What is the master password? ")
+key =load_key() + pwd.bytes
+fer =Fernet(key)
 
 def view():
     with open('password_manager/password.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("user: ", user, "| Password: ", passw)
+            print("user: ", user, "| Password: ",  str(fer.encrypt(passw.encode())))
 
 def add():
     user = input("Ente name: ")
     passw = input("Password: ")
     with open('password_manager/password.txt', 'a') as f:
-        f.write(user+ " | "+ passw + "\n")
+        f.write(user+ " | "+ str(fer.encrypt(passw.encode())) + "\n")
 
 while True:
     mode = input("Would you like to ad a new password or view existing ones(view/add/q) ").lower()
